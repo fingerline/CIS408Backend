fe336ccbae49fbb12d4d6c8ea01462bf0e6cb348
fingerline <trentfoxnorth@gmail.com>
2022-05-01 06:09:32 -0400

    cors support

--- CIS408Backend/settings/base.py
+++ CIS408Backend/settings/base.py
@@ -31,10 +31,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +65,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'CIS408Backend.wsgi.application'
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
